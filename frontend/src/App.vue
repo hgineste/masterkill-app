67a6e185097198b34a668d91f1c66ee5655cddf0
Hugo Gineste <gnsthugo@gmail.com>
2025-05-11 18:30:48 +0200

    Phase 3: Initial Vue.js frontend setup

---
+++ frontend/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div>
+      <h1>Masterkill Tracker</h1>
+    </div>
+  </header>
+  <main>
+    <RouterView /> </main>
+</template>
+
+<style scoped>
+/* Styles de base pour App.vue, vous pourrez les personnaliser */
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+  padding: 1rem;
+  border-bottom: 1px solid #ccc;
+}
+main {
+  padding: 1rem;
+}
+</style>
