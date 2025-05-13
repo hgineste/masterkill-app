ce0e9d5fd2e42a03d22fc12c67f1739b24feab61
Hugo Gineste <gnsthugo@gmail.com>
2025-05-13 14:29:44 +0200

    maj settings.py

--- backend/build.sh
+++ backend/build.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
-# exit on error
-set -o errexit
+set -o errexit # Arrête le script si une commande échoue
 
+echo "--- Running pip install ---"
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
-python manage.py migrate
+echo "--- Running manage.py migrate ---"
+python manage.py migrate # Exécute les migrations en premier
+
+echo "--- Running manage.py collectstatic ---"
+python manage.py collectstatic --no-input --clear # --clear pour nettoyer avant
+
+echo "--- Build script finished ---"
